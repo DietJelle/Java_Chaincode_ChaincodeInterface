0b9d19515f5a962793879114ced47d9e06d95d26
jellediet <jellediet@192.168.4.6>
2019-05-10 11:49:55 +0200

    Supports java classes with jackson

--- src/be/mentoringsystems/chaincode/Chaincode.java
+++ src/be/mentoringsystems/chaincode/Chaincode.java
@@ -5,8 +5,8 @@
  */
 package be.mentoringsystems.chaincode;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
@@ -24,7 +24,8 @@ public class Chaincode extends ChaincodeBase {
     public Response init(ChaincodeStub stub) {
         String fcn = stub.getFunction();
         List<String> params = stub.getParameters();
-        return newSuccessResponse("response message");
+        System.out.printf("init() %s %s\n", fcn, Arrays.toString(params.toArray()));
+        return newSuccessResponse();
     }
 
     /**
@@ -38,7 +39,7 @@ public class Chaincode extends ChaincodeBase {
         try {
             // Extract the function and args from the transaction proposal
             String func = stub.getFunction();
-            List<byte[]> params = stub.getArgs();
+            List<String> params = stub.getParameters();
             switch (func) {
                 case "set":
                     // Return result as success payload
@@ -60,72 +61,58 @@ public class Chaincode extends ChaincodeBase {
     }
 
     /**
-     * Get receives the value of a key from the ledger
+     * get receives the value of a key from the ledger
      *
      * @param stub {@link ChaincodeStub} to operate proposal and ledger
      * @param args key
      * @return Response with message and payload
      */
-    private Response get(ChaincodeStub stub, List<byte[]> args) {
+    private Response get(ChaincodeStub stub, List<String> args) {
         if (args.size() != 1) {
             throw new RuntimeException("Incorrect arguments. Expecting a key");
         }
 
-        String key = getKey(args.get(0));
-        byte[] value = stub.getState(key);
-        if (value == null) {
-            throw new RuntimeException("Asset not found: " + key);
+        String value = stub.getStringState(args.get(0));
+        if (value == null || value.isEmpty()) {
+            throw new RuntimeException("Asset not found: " + args.get(0));
         }
-        Response response = newSuccessResponse("Returned value for key : " + key, value);
+        Response response = newSuccessResponse("Returned value for key : " + args.get(0) + " = " + value, value.getBytes(StandardCharsets.UTF_8));
         return response;
     }
 
     /**
-     * Set stores the asset (both key and value) on the ledger. If the key
+     * set stores the asset (both key and value) on the ledger. If the key
      * exists, it will override the value with the new one
      *
      * @param stub {@link ChaincodeStub} to operate proposal and ledger
      * @param args key and value
      * @return value
      */
-    private Response set(ChaincodeStub stub, List<byte[]> args) {
+    private Response set(ChaincodeStub stub, List<String> args) {
         if (args.size() != 2) {
             throw new RuntimeException("Incorrect arguments. Expecting a key and a value");
         }
-        String key = getKey(args.get(0));
-        stub.putState(key, args.get(1));
-        return newSuccessResponse("Succesfully set key : " + key, args.get(1));
+        stub.putStringState(args.get(0), args.get(1));
+        return newSuccessResponse("Succesfully set key : " + args.get(0) + " as value : " + args.get(1), args.get(1).getBytes(StandardCharsets.UTF_8));
     }
 
     /**
-     * Delete the key from the world state
+     * Delete the key from the state in ledger
      *
      * @param stub {@link ChaincodeStub} to operate proposal and ledger
      * @param args key
      * @return Response with message and payload
      */
-    private Response delete(ChaincodeStub stub, List<byte[]> args) {
+    private Response delete(ChaincodeStub stub, List<String> args) {
         if (args.size() != 1) {
             return newErrorResponse("Incorrect number of arguments. Expecting a key");
         }
-        String key = getKey(args.get(0));
+        String key = args.get(0);
         // Delete the key from the state in ledger
         stub.delState(key);
-        return newSuccessResponse("Succesfully deleted key : " + key + "from the ledger", "success".getBytes(StandardCharsets.UTF_8));
+        return newSuccessResponse("Succesfully deleted key : " + args.get(0) + "from the ledger", args.get(0).getBytes(StandardCharsets.UTF_8));
     }
 
-    //Convert key to String
-    private String getKey(final byte[] keyBytes) {
-        String key;
-        try {
-            key = new String(keyBytes, "UTF-8");
-        } catch (UnsupportedEncodingException ex) {
-            throw new RuntimeException("UnsupportedEncodingException");
-        }
-        return key;
-    }
-
-    //Needs to be included in your main class defined in pom.xml
     public static void main(String[] args) {
 
         new Chaincode().start(args);
