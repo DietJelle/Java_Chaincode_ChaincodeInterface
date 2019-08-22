eac6cd42edff552f50e0d28e6721e514c476cb68
jellediet <jellediet@192.168.4.6>
2019-08-22 15:18:59 +0200

    Changed packages

--- src/be/blockchaindeveloper/chaincode/Chaincode.java
+++ src/be/blockchaindeveloper/chaincode/Chaincode.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package be.mentoringsystems.chaincode;
+package be.blockchaindeveloper.chaincode;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
